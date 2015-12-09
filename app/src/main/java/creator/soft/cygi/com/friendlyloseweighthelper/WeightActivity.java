46216d2ef7a8a2f3ad8f6a80eac91a715f1c07a3
JacekMistrzuKoder <jacek301@gmail.com>
2015-12-09 23:36:03 +0100

    Add new WeightActivity , WeightFragment , SingleFragmentActivity, Change code struktur to mach up MVC pattern

--- app/src/main/java/creator/soft/cygi/com/friendlyloseweighthelper/WeightActivity.java
+++ app/src/main/java/creator/soft/cygi/com/friendlyloseweighthelper/WeightActivity.java
@@ -1,33 +1,23 @@
 package creator.soft.cygi.com.friendlyloseweighthelper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
 
-public class WeightActivity extends AppCompatActivity {
+public class WeightActivity extends SingleFragmentActivity{
+
+    @Override
+    protected Fragment createFragment() {
+        return new WeightFragment();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weight);
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_weight, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
 
 
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
 }
---
+++ app/src/main/java/creator/soft/cygi/com/friendlyloseweighthelper/WeightFragment.java
@@ -0,0 +1,29 @@
+package creator.soft.cygi.com.friendlyloseweighthelper;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by CygiMasterProgrammer on 2015-12-09.
+ */
+public class WeightFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.weight_input_view,null);
+
+        return view;
+    }
+}
