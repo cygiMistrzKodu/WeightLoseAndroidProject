9a5fb6c9c4603a1fb2566249a858fc6fae319ed3
JacekMistrzuKoder <jacek301@gmail.com>
2015-12-16 00:58:53 +0100

    Create database schema

--- app/src/main/java/creator/soft/cygi/com/friendlyloseweighthelper/WeightTrackDatabaseHelper.java
+++ app/src/main/java/creator/soft/cygi/com/friendlyloseweighthelper/WeightTrackDatabaseHelper.java
@@ -3,6 +3,7 @@ package creator.soft.cygi.com.friendlyloseweighthelper;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by CygiMasterProgrammer on 2015-12-14.
@@ -18,6 +19,16 @@ public class WeightTrackDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("CREATE TABLE users (" +
+                   "id_user integer PRIMARY KEY AUTOINCREMENT,user_name VARCHAR(100))");
+
+        db.execSQL("CREATE TABLE measurement_data (" +
+                "measurement_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "id_user INTEGER REFERENCES users (id_user), " +
+                " date_time varchar(30),  weight real" );
+
+        Log.i("Baza", "Wykonalem tworzenie bazy ****");
 
     }
 
