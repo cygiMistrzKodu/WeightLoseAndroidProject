2b242fcf1898b0e87bf3c6ce98fb22d90082c38f
JacekMistrzuKoder <jacek301@gmail.com>
2016-01-16 23:42:20 +0100

    Testing with Test Testing now if I have any problem with Git when add new folder etc

---
+++ app/src/test/java/creator.soft.cygi.com.friendlyloseweighthelper/UnitTest.java
@@ -0,0 +1,15 @@
+package creator.soft.cygi.com.friendlyloseweighthelper;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by CygiMasterProgrammer on 2016-01-16.
+ */
+public class UnitTest {
+
+    @org.junit.Test
+    public void testUnitTest() throws Exception {
+        System.out.print("Just see if configuration is ok and working with Junit And GIT have no problems");
+    }
+
+}
